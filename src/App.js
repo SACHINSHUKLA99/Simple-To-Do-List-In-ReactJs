e99f3b69d71c2f6780d7ce8b410cd4b847e8230e
SACHINSHUKLA99 <33172331+SACHINSHUKLA99@users.noreply.github.com>
2020-01-23 19:17:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css'
 import Todos from './Todos';
 
-class App extends React.Component{
+class App extends Component{
   constructor(){
     super()
     this.state = {
